5675211a3a3c0e39ade369af54a1090ee9f194f5
jachias <jachias@localhost>
2024-10-01 16:50:58 +0200

    prueba

--- src/main/java/ProjectSpringBoot/HospitalApplication/Nurse.java
+++ src/main/java/ProjectSpringBoot/HospitalApplication/Nurse.java
@@ -1,5 +1,29 @@
 package ProjectSpringBoot.HospitalApplication;
 
 public class Nurse {
+	private String name;
+    private int id;
+    
+    public Nurse(String name, int id) {
+    	this.name = name;
+    	this.id = id; 
+    }
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String nombre) {
+		this.name = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+    
 }
+
