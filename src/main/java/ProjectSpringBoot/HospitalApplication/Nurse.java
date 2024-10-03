b5cceca5e5b6558b6129c2e6087d9198b47390f4
Gerard <gerard@example.com>
2024-10-03 16:39:42 +0200

    Cambios definitivos

--- src/main/java/ProjectSpringBoot/HospitalApplication/Nurse.java
+++ src/main/java/ProjectSpringBoot/HospitalApplication/Nurse.java
@@ -1,5 +1,41 @@
 package ProjectSpringBoot.HospitalApplication;
 
 public class Nurse {
+		
+	private String name;
+	private String username;
+	private String password;
 
+	public Nurse(String nombre, String username, String password) {
+	        
+	    this.name = nombre;
+	    this.username = username;
+	    this.password = password;
+	
+	}
+
+	public String getNombre() {
+	    return name;
+	}
+
+	public void setNombre(String nombre) {
+	    this.name = name;
+	}
+
+	public String getUsername() {
+	    return username;
+	}
+
+	public void setUsername(String username) {
+	    this.username = username;
+	}
+
+	public String getPassword() {
+	    return password;
+	}
+
+	public void setPassword(String password) {
+	    this.password = password;
+	}
+	
 }
--- src/main/java/ProjectSpringBoot/HospitalApplication/NurseController.java
+++ src/main/java/ProjectSpringBoot/HospitalApplication/NurseController.java
@@ -1,7 +1,35 @@
 package ProjectSpringBoot.HospitalApplication;
 
-public class NurseController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-	
-	
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+class NurseController {
+
+    private List<Nurse> nurses = new ArrayList<>();
+
+    @Autowired
+    public NurseController() {
+        nurses.add(new Nurse("Juan Pérez", "juan.perez", "pass1"));
+        nurses.add(new Nurse("Maria Lopez", "maria.lopez", "pass2"));
+        nurses.add(new Nurse("Antonio Garrido", "antonio.garrido", "pass3"));
+        nurses.add(new Nurse("Carlos Gonzalez", "carlos.gonzalez", "pass4"));
+        nurses.add(new Nurse("Sandra Castillo", "sandra.castillo", "pass5"));
+    }
+
+    @PostMapping("/nurse/login")
+    public @ResponseBody ResponseEntity<Boolean> login(@RequestBody Nurse inputNurse) {
+        for (Nurse nurse : nurses) {
+        	System.out.println(nurse);
+            if (inputNurse.getUsername().equals(nurse.getUsername()) && inputNurse.getPassword().equals(nurse.getPassword())) {
+                return ResponseEntity.ok(true);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+    }
 }
