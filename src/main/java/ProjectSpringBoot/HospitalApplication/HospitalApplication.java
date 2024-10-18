35fee2bf117e3027bd21885bbb391a791e6e081a
Gerard <gerard@example.com>
2024-10-18 19:20:44 +0200

    Changes

--- src/main/java/ProjectSpringBoot/HospitalApplication/HospitalApplication.java
+++ src/main/java/ProjectSpringBoot/HospitalApplication/HospitalApplication.java
@@ -1,37 +1,35 @@
 package ProjectSpringBoot.HospitalApplication;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
+import org.springframework.context.ApplicationContext;
 
 import java.util.List;
-import java.util.ArrayList;
-import ProjectSpringBoot.HospitalApplication.NurseController;
 import java.util.Scanner;
+
 @SpringBootApplication
-
-
 public class HospitalApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(HospitalApplication.class, args);
-      NurseController nurseController = new NurseController();
+        ApplicationContext context = SpringApplication.run(HospitalApplication.class, args);
+
+        NurseController nurseController = context.getBean(NurseController.class);
+
         List<Nurse> nurses = nurseController.getAll();
         for (Nurse nurse : nurses) {
             System.out.println(nurse);
-      
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Ingresa el nombre del enfermero:");
-		String nurseName = scanner.nextLine();
-		
-		ResponseEntity<Nurse> foundNurse = nurseController.findByName(nurseName);
-	    if (foundNurse != null) {
-	        System.out.println("Enfermero encontrado: " + foundNurse);
-	    } else {
-	        System.out.println("Enfermero no encontrado");
-	    }
         }
-   
-   }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa el nombre del enfermero:");
+        String nurseName = scanner.nextLine();
+
+        var foundNurse = nurseController.findByName(nurseName);
+        if (foundNurse.getBody() != null) {
+            System.out.println("Enfermero encontrado: " + foundNurse.getBody());
+        } else {
+            System.out.println("Enfermero no encontrado");
+        }
+        
+    }
 }
