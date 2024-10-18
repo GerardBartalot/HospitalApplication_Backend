a1eacd14d07fbcd5d98d523c3b78bf7f9b60c06e
jachias <jachias@localhost>
2024-10-18 16:28:30 +0200

    issue #14 completed

--- src/main/java/ProjectSpringBoot/HospitalApplication/NurseController.java
+++ src/main/java/ProjectSpringBoot/HospitalApplication/NurseController.java
@@ -6,31 +6,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/nurse")
 public class NurseController {
 
-    @Autowired
-    private NurseRepository nurseRepository;
+	@Autowired
+	private NurseRepository nurseRepository;
 
-    @PostMapping("/login")
-    public ResponseEntity<String> validateLogin(@RequestParam String username, @RequestParam String password) {
-        Nurse nurse = nurseRepository.findByUsername(username);
-        if (nurse != null && nurse.getPassword().equals(password)) {
-            return new ResponseEntity<>("Login Successful", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
-        }
-    }
-
-	public ResponseEntity<Nurse> findByName(String nurseName) {
-		return null;
+	@PostMapping("/login")
+	public ResponseEntity<String> validateLogin(@RequestParam String username, @RequestParam String password) {
+		Nurse nurse = nurseRepository.findByUsername(username);
+		if (nurse != null && nurse.getPassword().equals(password)) {
+			return new ResponseEntity<>("Login Successful", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
+		}
 	}
 
-	public List<Nurse> getAll() {
-		return null;
+	@GetMapping("/find")
+	public ResponseEntity<Nurse> findByName(@RequestParam String name) {
+		Nurse nurse = nurseRepository.findByName(name);
+			if (nurse.getName().equalsIgnoreCase(name)) {
+				return ResponseEntity.ok(nurse); // 200
+			}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // 404
 	}
 
-    
 }
--- src/main/java/ProjectSpringBoot/HospitalApplication/NurseRepository.java
+++ src/main/java/ProjectSpringBoot/HospitalApplication/NurseRepository.java
@@ -1,6 +1,7 @@
 package ProjectSpringBoot.HospitalApplication;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NurseRepository extends CrudRepository<Nurse, Integer> {
     Nurse findByUsername(String username);
+    Nurse findByName(String name);
     List<Nurse> findByNameContainingIgnoreCase(String name);
 }
