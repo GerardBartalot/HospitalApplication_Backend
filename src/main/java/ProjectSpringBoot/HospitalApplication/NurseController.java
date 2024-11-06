bb32ea74344f4ca4019657c7c187f883c28e8409
Gerard <gerard@example.com>
2024-11-06 16:19:43 +0100

    Actualizar Enfermero hecho

--- src/main/java/ProjectSpringBoot/HospitalApplication/NurseController.java
+++ src/main/java/ProjectSpringBoot/HospitalApplication/NurseController.java
@@ -38,12 +38,36 @@ public class NurseController {
 		return (List<Nurse>) nurseRepository.findAll();
 	}
 
-	@GetMapping("/name")
-	public ResponseEntity<Nurse> findByName(@RequestParam String name) {
-		Nurse nurse = nurseRepository.findByName(name);
-		if (nurse.getName().equalsIgnoreCase(name)) {
-			return ResponseEntity.ok(nurse); // 200
-		}
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // 404
+	@GetMapping("/name/{name}")
+	public ResponseEntity<Nurse> findByName(@PathVariable String name) {
+	    Nurse nurse = nurseRepository.findByName(name);
+	    if (nurse.getName().equalsIgnoreCase(name)) {
+	        return ResponseEntity.ok(nurse); // 200
+	    }
+	    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // 404
 	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<String> updateNurse(@PathVariable int id, @RequestBody Nurse updatedNurse) {
+	    Optional<Nurse> existingNurse = nurseRepository.findById(id);
+
+	    if (existingNurse.isPresent()) {
+	        Nurse nurse = existingNurse.get();
+
+	        if (updatedNurse.getName() != null) {
+	            nurse.setName(updatedNurse.getName());
+	        }
+	        if (updatedNurse.getUsername() != null) {
+	            nurse.setUsername(updatedNurse.getUsername());
+	        }
+	        if (updatedNurse.getPassword() != null) {
+	            nurse.setPassword(updatedNurse.getPassword());
+	        }
+
+	        nurseRepository.save(nurse);
+	        return new ResponseEntity<>("Nurse updated successfully", HttpStatus.OK);
+	    } else {
+	        return new ResponseEntity<>("Nurse not found", HttpStatus.NOT_FOUND);
+	    }
+	
 }
