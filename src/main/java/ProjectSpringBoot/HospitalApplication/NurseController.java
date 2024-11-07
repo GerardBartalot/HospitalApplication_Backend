005feb56aa8a143d91f42fb4661c3f7654f443a7
tarde <tarde@10.0.22.100>
2024-11-07 17:07:22 +0100

    issue #19 Create done from CRUD

--- src/main/java/ProjectSpringBoot/HospitalApplication/NurseController.java
+++ src/main/java/ProjectSpringBoot/HospitalApplication/NurseController.java
@@ -22,6 +22,27 @@ public class NurseController {
 
 	private NurseRepository nurseRepository;
 
+	@PostMapping("/create")
+	public ResponseEntity<String> create(@RequestBody Nurse newNurse) {
+	    if (newNurse.getName() == null || newNurse.getName().isEmpty() ||
+	        newNurse.getUsername() == null || newNurse.getUsername().isEmpty() ||
+	        newNurse.getPassword() == null || newNurse.getPassword().isEmpty()) {
+	    	
+	        
+	        return new ResponseEntity<>("All fields (id, name, username, and password) are required.", HttpStatus.BAD_REQUEST);
+	    }
+
+	    Nurse savedNurse = nurseRepository.save(newNurse);
+
+	    if (savedNurse != null) {
+	        return new ResponseEntity<>("Nurse added successfully with ID: " + savedNurse.getNurse_id(), HttpStatus.CREATED);
+	    } else {
+	        return new ResponseEntity<>("Error creating nurse", HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	}
+
+	
+	
 	@PostMapping("/login")
 
 	public ResponseEntity<String> validateLogin(@RequestParam String username, @RequestParam String password) {
