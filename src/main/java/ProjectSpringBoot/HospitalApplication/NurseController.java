b12c8506871a31c8e3e0412ca31852fdd909fc6a
jachias <jachias@localhost>
2024-11-07 16:35:31 +0100

    Encontrar nurse por id hecho #20

--- src/main/java/ProjectSpringBoot/HospitalApplication/NurseController.java
+++ src/main/java/ProjectSpringBoot/HospitalApplication/NurseController.java
@@ -35,15 +35,51 @@ public class NurseController {
 
 	@GetMapping("/index")
 	public List<Nurse> getAll() {
-		return nurseRepository.findAll();
+		return (List<Nurse>) nurseRepository.findAll();
 	}
 
-	@GetMapping("/name")
-	public ResponseEntity<Nurse> findByName(@RequestParam String name) {
+	@GetMapping("/name/{name}")
+	public ResponseEntity<Nurse> findByName(@PathVariable String name) {
 		Nurse nurse = nurseRepository.findByName(name);
 		if (nurse.getName().equalsIgnoreCase(name)) {
 			return ResponseEntity.ok(nurse); // 200
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // 404
+	}
+	
+	@GetMapping("/read/{id}")
+    public ResponseEntity<Nurse> getNurseById(@PathVariable int id) {
+        Optional<Nurse> nurse = nurseRepository.findById(id);
+        if (nurse.isPresent()) {
+            return new ResponseEntity<>(nurse.get(), HttpStatus.OK);//200
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);//404
+        }
+    }
+
+
+	@PutMapping("/update/{id}")
+	public ResponseEntity<String> updateNurse(@PathVariable int id, @RequestBody Nurse updatedNurse) {
+		Optional<Nurse> existingNurse = nurseRepository.findById(id);
+
+		if (existingNurse.isPresent()) {
+			Nurse nurse = existingNurse.get();
+
+			if (updatedNurse.getName() != null) {
+				nurse.setName(updatedNurse.getName());
+			}
+			if (updatedNurse.getUsername() != null) {
+				nurse.setUsername(updatedNurse.getUsername());
+			}
+			if (updatedNurse.getPassword() != null) {
+				nurse.setPassword(updatedNurse.getPassword());
+			}
+
+			nurseRepository.save(nurse);
+			return new ResponseEntity<>("Nurse updated successfully", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("Nurse not found", HttpStatus.NOT_FOUND);
+		}
+
 	}
 }
