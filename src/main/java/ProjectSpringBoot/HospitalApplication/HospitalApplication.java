78975d56c94816ba62f56a1b9bd9bb66ddd43b97
Gerard Bartalot <167296850+GerardBartalot@users.noreply.github.com>
2024-11-08 18:21:54 +0100

    Update HospitalApplication.java

--- src/main/java/ProjectSpringBoot/HospitalApplication/HospitalApplication.java
+++ src/main/java/ProjectSpringBoot/HospitalApplication/HospitalApplication.java
@@ -1,17 +1,12 @@
 package ProjectSpringBoot.HospitalApplication;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.context.ApplicationContext;
-
 import java.util.Scanner;
 
 @SpringBootApplication
-
 public class HospitalApplication {
-
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(HospitalApplication.class, args);
 	}
